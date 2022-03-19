67809c230297bc470d0fbfab38c493380f7a88e0
manojmahanta25 <manojmahanta25@gmail.com>
2022-03-19 20:14:10 +0530

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  content: ["./src/**/*.{html,js}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
