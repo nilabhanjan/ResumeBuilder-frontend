cdd793999072851826b6836b193d09060020d4c4
manojmahanta25 <manojmahanta25@gmail.com>
2022-03-20 11:54:49 +0530

    resume half done

--- src/app/component/middle/middle.component.ts
+++ src/app/component/middle/middle.component.ts
@@ -7,15 +7,41 @@ import {ClassService} from "../../../services/class.service";
   templateUrl: './middle.component.html',
   styleUrls: ['./middle.component.scss']
 })
-export class MiddleComponent implements OnInit,OnDestroy {
+export class MiddleComponent implements OnInit, OnDestroy {
 
-  darkMode:boolean=false
-  subscription:Subscription
-  constructor(private classService:ClassService) {
-    this.subscription=classService.getDarkMode().subscribe((value)=>{
+  darkMode: boolean = false
+  subscription: Subscription
+  dataArray:{title:string,icon:string,description:string}[] = [
+    {
+      title: 'About',
+      icon: 'gg-profile',
+      description: 'Freshly Graduate in Master of Science Information Technology looking for an opportunity to move ahead in career.'
+    },
+    {
+      title: 'Education',
+      icon: 'gg-organisation',
+      description: 'Freshly Graduate in Master of Science Information Technology looking for an opportunity to move ahead in career.'
+    },
+    {
+      title: 'Goals',
+      icon: 'gg-file-document',
+      description: 'Freshly Graduate in Master of Science Information Technology looking for an opportunity to move ahead in career.'
+    },
+    {
+      title: 'Address',
+      icon: 'gg-info',
+      description: 'Freshly Graduate in Master of Science Information Technology looking for an opportunity to move ahead in career.'
+    }
+
+
+  ]
+
+  constructor(private classService: ClassService) {
+    this.subscription = classService.getDarkMode().subscribe((value) => {
       this.darkMode = value
     })
   }
+
   ngOnDestroy() {
     this.subscription.unsubscribe()
   }
--- src/services/class.service.ts
+++ src/services/class.service.ts
@@ -6,6 +6,13 @@ import {BehaviorSubject} from "rxjs";
 })
 export class ClassService {
   private darkMode:BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false)
+  private DARK_MODE_KEY='dark_mode'
+  constructor() {
+    let darkModeLocalStorage = this.getLocalStorageKeyStorage(this.DARK_MODE_KEY)
+    if(darkModeLocalStorage){
+      this.darkMode.next(Boolean(darkModeLocalStorage))
+    }
+  }
   getDarkMode(){
     return this.darkMode
   }
@@ -13,5 +20,16 @@ export class ClassService {
    let value = this.darkMode.value
     value = !value
     this.darkMode.next(value)
+    this.setKeyValueToLocalStorage(this.DARK_MODE_KEY,value)
   }
+  setKeyValueToLocalStorage(key:string,value:any){
+    localStorage.setItem(key, value)
+  }
+  getLocalStorageKeyStorage(key:string){
+    return localStorage.getItem(key)
+  }
+  removeLocalStorageItem(key:string){
+    localStorage.removeItem(key)
+  }
+
 }
