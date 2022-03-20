681a99ca9920cc7c5f9a28fd2346ab4edea2f1bb
NILABH <nilavanjanchutia@gmail.com>
2022-03-20 19:47:01 +0530

    removed reds

--- src/app/component/blog/blog.component.ts
+++ src/app/component/blog/blog.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {ClassService} from "../../../services/class.service";
-import {Subscription} from "rxjs";
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-blog',
